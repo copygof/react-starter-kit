b772927001ecf9499675a8a35df314a1c84fa616
Preecha Sattabut <precha.sattabut@gmail.com>
2019-11-23 00:24:46 +0700

    Add some lib

--- src/app/rootReducer.js
+++ src/app/rootReducer.js
@@ -1,9 +1,8 @@
 import { combineReducers } from 'redux'
-import counter from '../features/counter/counterSlice'
-import todos from '../features/todo/todoSlice'
-import product from '../features/product/productSlice'
-import cart from '../features/cart/cartSlice'
-// import product from '../features/product/productSlice'
+import counter from 'features/counter/counterSlice'
+import todos from 'features/todo/todoSlice'
+import product from 'features/product/productSlice'
+import cart from 'features/cart/cartSlice'
 
 export default combineReducers({
   counter,
